4708dc06ec82a1f14596d4aa6d2a9b7da0de31d4
Richard Ashley <rashley@gmail.com>
2023-11-15 11:51:47 +0000

    added support for queuing of data when connection to serveris down.

--- loggers.py
+++ loggers.py
@@ -21,10 +21,14 @@ class webLogger():
 			else: local=False
 		except KeyError: local=False
 			
-		try: 
-			self.postURL = config['URL']
-		except KeyError:
-			self.postURL = "https://localhost:8080/postmeteo"
+		try: self.postURL = config['URL']
+		except KeyError: self.postURL = "https://localhost:8080/postmeteo"
+
+		try: self.queuePostURL = config['queuePostURL']
+		except KeyError: self.postURL = "https://localhost:8080/postqueuedmeteo"
+
+		try: self.logQueueFile = config['logQueueFile']
+		except KeyError: self.logQueueFile = "/var/log/meteo_queue.log"
 		
 		if local: self.postURL = config['localURL']
 
@@ -37,13 +41,36 @@ class webLogger():
 		while not self.exit:
 			timeStamp = datetime.datetime.now()
 			timeStampStr = timeStamp.strftime("%Y-%m-%d %H:%M:%S")
-			logData = {"timestamp": timeStampStr, "hostname": self.hostname, "json" : {} }
 			
+			# Upload any queued readings
+			if os.path.exists(self.logQueueFile):
+				print("uploading some queued data to %s"%self.queuePostURL, flush=True)
+				queue = open(self.logQueueFile, 'rt')
+				jsonArray = []
+				for line in queue:
+					jsonArray.append(json.loads(line))
+				print(jsonArray, flush=True)
+				if self.sendData(self.postURL, jsonArray):
+					# Data uploaded ok, we can clean up the queued data
+					print("queued data uploaded.... cleaning up the queue.", flush=True)
+					os.remove(self.logQueueFile)
+
+			logData = {"timestamp": timeStampStr, "hostname": self.hostname, "json" : {} }
 			for s in self.sensors:
 				logData['json'][s.name] = s.logData
 			debug("uploading to %s: "%self.postURL + json.dumps(logData))
-			if not self.sendData(self.postURL, logData):
+			rows = [ logData ]
+			if not self.sendData(self.postURL, rows):
 				print("ERROR in uploading data!", flush=True)
+				# Write to a log queue
+				if not os.path.exists(self.logQueueFile): queue = open(self.logQueueFile, "wt")
+				else: queue = open(self.logQueueFile, "at")
+				json.dump(logData, queue)
+				queue.write("\n")
+				queue.close()
+
+
+
 			time.sleep(self.monitorCadence)
 		print("%s monitor stopped."%self.name)
 	
@@ -57,6 +84,7 @@ class webLogger():
 				print("Data not uploaded.")
 				return False
 			responseJSON = json.loads(response.text)
+			print("Response from db server:", responseJSON['status'], flush=True)
 			if responseJSON['status'] == 'OK': success = True
 			response.close()
 		except Exception as e: 
